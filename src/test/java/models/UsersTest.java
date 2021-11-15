019fde436979d47b9742b0c01aa530e7089d2d82
wdmuthiora <wdmuthiora@gmail.com>
2021-11-15 23:04:27 +0300

    Generated Classes and their respective test files.

---
+++ src/test/java/models/UsersTest.java
@@ -0,0 +1,17 @@
+package models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
