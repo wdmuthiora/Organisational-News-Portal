9d30e5b870a8d45c0d1f0bbc0fc237232345896d
wdmuthiora <wdmuthiora@gmail.com>
2021-11-16 00:23:26 +0300

    Tested that News instantiates correctly.

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,19 @@
 package models;
 
 public class News {
+    private int id;
+    private String newsType;
+    private int departmentId;
+    private int userId;
+    private String title;
+    private String description;
+    private final String TYPE_OF_NEWS="general";
+
+    public News(int id, String newsType, int departmentId, String title, String description) {
+        this.id=  id;
+        this.newsType = newsType;
+        this.departmentId = departmentId;
+        this.title = title;
+        this.description = description;
+    }
 }
--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -2,7 +2,10 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NewsTest {
@@ -14,4 +17,16 @@ class NewsTest {
     @AfterEach
     void tearDown() {
     }
+    @Test
+    @DisplayName("News Instantiates Correctly.")
+    public void testInstanceOfNews_true(){
+        News testNews = setUpNews();
+        assertEquals(true, testNews instanceof News);
+    }
+
+ //helper
+    private News setUpNews() {
+        return new News (1, "Blog", 1, "news tonight","same ol'nonesense");
+    }
+
 }
