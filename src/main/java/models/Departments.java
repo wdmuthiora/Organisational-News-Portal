78baa96a2a04b631e20006e4d6e4d06270782772
wdmuthiora <wdmuthiora@gmail.com>
2021-11-16 20:29:15 +0300

    Tested that Departments initializes correctly.

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,4 +1,13 @@
 package models;
 
 public class Departments {
+    private String name;
+    private String description;
+    private int id;
+    private int size;
+
+    public Departments(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
--- src/test/java/models/DepartmentsTest.java
+++ src/test/java/models/DepartmentsTest.java
@@ -2,7 +2,10 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepartmentsTest {
@@ -13,5 +16,17 @@ class DepartmentsTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    @DisplayName("Departments Instantiates Correctly.")
+    public void testInstanceOfAnimal_true(){
+        Departments testDepartments = setUpDepartments();
+        assertEquals(true, testDepartments instanceof Departments);
+    }
+
+    //helper
+    private Departments setUpDepartments() {
+        return new Departments ("Betty", "lawyer");
     }
 }
